4f5c79311f00a0533080d7f65654b8563234d36d
vcarlsberg <v.carlsberg@gmail.com>
2019-10-06 18:42:17 +0000

    - including dev off to reset plotting area - changing filtering process explanation to Indonesian - Change method of removing punctuation into more aggresive - Analyzing sentiment using ruleSentiment, since we're applyting custom Dictionary Binary - Adding caption on plot

--- crawl_twitter_indo.R
+++ crawl_twitter_indo.R
@@ -5,31 +5,34 @@ library(wordcloud)
 library(SentimentAnalysis)
 library(syuzhet)
 library(data.table)
-library(textstem)
 
 twitter_user="@Lambe_Turah"
 
 setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
 tweet_data = userTimeline(twitter_user,n=1000)
 
-df <- do.call("rbind", lapply(tweet_data, as.data.frame))
+df<-twListToDF(tweet_data)
 dim(df)
 
 corpus <- SimpleCorpus(VectorSource(df$text))
-# 1. Stripping any extra white space:
+# 1. Menghilangkan whitespace
 corpus <- tm_map(corpus, stripWhitespace)
-# 2. Transforming everything to lowercase
+# 2. Konversi ke lowercase
 corpus <- tm_map(corpus, content_transformer(tolower))
-# 3. Removing numbers 
+# 3. Menghilangkan angka
 corpus <- tm_map(corpus, removeNumbers)
-# 4. Removing punctuation
-corpus <- tm_map(corpus, removePunctuation)
+# 4. Menghilangkan tanda baca
+#corpus <- tm_map(corpus, removePunctuation)
+removeSymbols <- function(x) gsub("[^[:alpha:][:space:]]*","",x)
+corpus <- tm_map(corpus, content_transformer(removeSymbols))
+
 # 5. Removing stop words
 a<-read.delim("id.stopwords.02.01.2016.txt")
 corpus <- tm_map(corpus, removeWords, a$STOPWORDS)
 
 DTM <- DocumentTermMatrix(corpus,
-                          control = list(removePunctuation = TRUE,stopwords = TRUE))
+                          control = list(removePunctuation = TRUE,stopwords = TRUE)
+                          )
 inspect(DTM)
 
 sums <- as.data.frame(colSums(as.matrix(DTM)))
@@ -37,6 +40,7 @@ sums <- rownames_to_column(sums)
 colnames(sums) <- c("term", "count")
 sums <- arrange(sums, desc(count))
 #head <- sums[1:75,]
+dev.off()
 wordcloud(words = sums$term, freq = sums$count, min.freq = 10, 
           max.words=100, random.order=FALSE, rot.per=0.35,
           colors=brewer.pal(8, "Dark2"))
@@ -45,22 +49,33 @@ neg=scan("negative.txt",character(),quote="")
 pos=scan("positive.txt",character(),quote="")
 
 d <- SentimentDictionaryBinary(neg,pos)
-summary(d)
+#summary(d)
+#print(d)
 
-
-sent <- analyzeSentiment(DTM, language=d)
+#ruleSentiment(DTM, d)
+sent<-ruleSentiment(DTM,d)
+#sent <- analyzeSentiment(DTM, rules=ruleSentiment(DTM, d))
 # were going to just select the Harvard-IV dictionary results ..  
-sent <- sent[,1:4]
+#sent <- sent[,1:4]
 #Organizing it as a dataframe
 sent <- as.data.frame(sent)
 # Now lets take a look at what these sentiment values look like. 
 #head(sent)
-summary(sent$SentimentGI)
+summary(sent)
 
-sent$sentimentGIx<-cut(sent$SentimentGI,breaks=c(-1,-0.000001,0.000001,1),
-                       labels=c("Negative","Neutral","Positive"))
-
-ggplot(data=sent,aes(sentimentGIx))+
+sent$sentiment<-cut(sent$sent,breaks=c(-1,-0.5,-0.25,-0.000001,0.000001,0.25,0.5,1),
+                       labels=c("Strongly Negative",
+                                "Negative",
+                                "Mildly Negative",
+                                "Neutral",
+                                "Mildly Positive",
+                                "Positive",
+                                "Strongly Positive"))
+dev.off()
+ggplot(data=sent,aes(sentiment))+
   geom_bar(position="stack")+
-  scale_fill_grey()
+  scale_fill_grey()+
+  labs(title =paste("Distribusi Sentimen Tweet",twitter_user,sep=" "), 
+       x = "Kategori Sentimen", y = "Jumlah Tweet") +
+  theme(plot.title = element_text(hjust = 0.5))
 
