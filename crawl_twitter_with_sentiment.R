7e7359d31669ba7944b8582b5e107316f58a411e
vcarlsberg <v.carlsberg@gmail.com>
2019-10-11 12:18:59 +0000

    - change twitter into detikcom

--- crawl_twitter_with_sentiment.R
+++ crawl_twitter_with_sentiment.R
@@ -5,20 +5,19 @@ library(wordcloud)
 library(SentimentAnalysis)
 library(syuzhet)
 library(data.table)
+library(textstem)
 
 api_key="oJmTP5bhCgutk4aQNOvZb4RtG"
 api_secret="z51jwqM7kirUKBId7GJ07SMaTxeNtZk4xlrfyUpILO2JKXJMas"
 access_token="48937719-ciXusxAAYXZCKvKD1e0P58GxIw7D9DiR7JdkZR2lk"
 access_token_secret="Rk5IlscyUxQM4DJ9j6jBeIE7mtfuPWNvxLLMV2muoZ9HX"
-twitter_user="@FoxNews"
+twitter_user="@Jokowi"
 
 setup_twitter_oauth(api_key, api_secret, access_token, access_token_secret)
-tweet_data = userTimeline(twitter_user,n=500)
+tweet_data = userTimeline(twitter_user,n=10)
 
 df <- do.call("rbind", lapply(tweet_data, as.data.frame))
 dim(df)
-
-
 
 corpus <- SimpleCorpus(VectorSource(df$text))
 # 1. Stripping any extra white space:
@@ -30,11 +29,16 @@ corpus <- tm_map(corpus, removeNumbers)
 # 4. Removing punctuation
 corpus <- tm_map(corpus, removePunctuation)
 # 5. Removing stop words
-corpus <- tm_map(corpus, removeWords, c(stopwords("english"),"jakpost","dari","sejak","dengan","lagi",
-                                        "dan","juga","selalu","karena",
-                                        "ada","yang","kamu","ini","itu","saja","bisa"))
+corpus <- tm_map(corpus, removeWords, stopwords("english"))
 
-DTM <- DocumentTermMatrix(corpus)
+
+
+
+lemma_word=lemmatize_words(corpus)
+lemma_word
+
+DTM <- DocumentTermMatrix(corpus,
+                          control = list(removePunctuation = TRUE,stopwords = TRUE))
 inspect(DTM)
 
 sums <- as.data.frame(colSums(as.matrix(DTM)))
@@ -52,7 +56,7 @@ sent <- sent[,1:4]
 #Organizing it as a dataframe
 sent <- as.data.frame(sent)
 # Now lets take a look at what these sentiment values look like. 
-head(sent)
+#head(sent)
 summary(sent$SentimentGI)
 
 sent2 <- get_nrc_sentiment(df$text)
